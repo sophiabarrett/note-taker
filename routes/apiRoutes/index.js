86ccb7da4f0e946676c9a019a73b011997010df3
Sophia Barrett <sophia@yoursummit.media>
2021-11-26 07:14:21 -0700

    setup api post route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
+const createNote = require('../../lib/notes');
 const notes = require('../../db/db.json');
+const uniqid = require('uniqid');
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    // add unique id
+    req.body.id = uniqid();
+
+    // add note to db
+    const newNote = createNote(req.body, notes);
+    res.json(newNote);
 });
 
 module.exports = router;
