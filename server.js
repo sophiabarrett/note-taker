746b21761d80fe2cb33bcdf3ead6fad4f98590cb
Sophia Barrett <sophia@yoursummit.media>
2021-11-26 06:57:08 -0700

    setup html and api get routes

---
+++ server.js
@@ -0,0 +1,16 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// setup routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`🚀 API server now listening on port ${PORT}! 🚀`);
+});
